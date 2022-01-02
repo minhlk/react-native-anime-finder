d49a9c7d76c76c68c8279645fc6d7071b8a3179f
minhbd <minhbd@pascaliaasia.com>
2022-01-02 10:40:39 +0700

    [ADD] Layout, jikan API, Simple Actions

---
+++ api/jikan/lib/Request.js
@@ -0,0 +1,24 @@
+
+
+module.exports = class Request {
+  constructor() {
+    this.baseURL = "https://api.jikan.moe/v3"
+  }
+
+  async send(args, params) {
+    var res = await fetch(this.createUrl(args, params))
+    var data = await res.json()
+    if (res.status !== 200) throw `Response: ${res.status}`
+    else return data
+  }
+
+  createUrl(args, params) {
+    const url = new URL(this.baseURL)
+    url.pathname += `/${args.filter(a => a).join("/")}`
+    for (let p in params) {
+      url.searchParams.set(p, params[p])
+    }
+    console.log(url.href)
+    return url.href
+  }
+}
