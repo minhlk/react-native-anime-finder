3083bd35155581f30e4e6455a1a3edbe3d6202ca
minhlk <duyminhdev@gmail.com>
2022-01-09 13:36:24 +0700

    [ADD] Redux & redux thunk

--- containers/Home/Home.tsx
+++ containers/Home/Home.tsx
@@ -6,12 +6,10 @@ import {
   ActivityIndicator,
   TextInput
 } from 'react-native';
-import { getAnimesByName } from '../../api/api.js';
 import ListView from '../../components/ListView/ListView.js';
-
-interface AnimeList {
-  results : Array<AnimeInfo>
-}
+import { connect } from 'react-redux';
+import { fetchAnime } from '../../actions/fetchAnime';
+import { bindActionCreators } from 'redux';
 
 interface AnimeInfo {
   mal_id: number,
@@ -21,29 +19,16 @@ interface AnimeInfo {
   synopsis: string
 }
 
-const Home = ({ navigation } : any) => {
-  const [items, setItems] = useState<Array<AnimeInfo>>([]);
+const Home = ( props: any) => {
+  const { navigation } = props;
   const [searchQuery, setSearchQuery] = useState('gal');
-  const [isLoading, setLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
   
-  const searchAnime = () => {
-    getAnimesByName(searchQuery, (res: AnimeList) => {
-      console.log(res)
-      setItems(res.results);
-      setLoading(false);
-    }, (_: any) => {
-      setIsError(true);
-      setLoading(false);
-    });
-  }
   useEffect(() => {
-    searchAnime()
+    props.fetchAnime(searchQuery);
   }, []);
 
   const onSubmit = () => {
-      setLoading(true);
-      searchAnime();
+    props.searchAnime(searchQuery);
   };
 
   const convertToCommonItem = (item: AnimeInfo) => {
@@ -59,8 +44,10 @@ const Home = ({ navigation } : any) => {
     navigation.navigate('Details', { id: item.id, title: item.title });
   }
 
-  if (isLoading) return <ActivityIndicator />;
-  if (isError) return (<View><Text>Fail to fetch data</Text></View>);
+  if (props.isFetching) {
+    return <ActivityIndicator/>
+  }
+  if (props.isError) return (<View><Text>Fail to fetch data</Text></View>);
 
   return (
     <View style={{ height: '100%' }}>
@@ -71,12 +58,10 @@ const Home = ({ navigation } : any) => {
         value={searchQuery}
         style={styles.searchBar}
       />
-      <ListView items={items} convertToCommonItem={convertToCommonItem} onItemPressed={onItemPressed} />
+      <ListView items={props.animeList} convertToCommonItem={convertToCommonItem} onItemPressed={onItemPressed} />
     </View>
   );
 };
-
-export default Home;
 
 const styles = StyleSheet.create({
   searchBar: {
@@ -84,3 +69,15 @@ const styles = StyleSheet.create({
     margin:10,
   }
 });
+
+const mapStateToProps = state => ({
+  animeList: state.fetchAnime.animeList,
+  isFetching: state.fetchAnime.isFetching,
+  isError: state.fetchAnime.isError
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchAnime: bindActionCreators(fetchAnime, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
